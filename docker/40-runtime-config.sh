c48b396ddeff34f5658ba377e3ef0772ada399bc
Suzan Al-Majdalawi <suzan.al-majdalawi@chasacademy.se>
2026-09-25 13:37:32 +0200

    Add:FEATURE_NORWAY

--- docker/40-runtime-config.sh
+++ docker/40-runtime-config.sh
@@ -13,4 +13,4 @@ cat > /usr/share/nginx/html/config.js <<CONF
 window.__KRAFTLY__ = { env: '${APP_ENV:-lokal}' }
 CONF
 
-echo "runtime-config: APP_ENV=${APP_ENV:-lokal}"
+echo "runtime-config: APP_ENV=${APP_ENV:-lokal} FEATURE_NORWAY=${NORWAY}"
